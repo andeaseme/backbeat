4b6c1348acf368ec4c264b662e307c0098b67948
Andie Shining Phan <aphan@student.42.us.org>
2017-08-16 22:35:21 -0700

    consumer transcodes and buckets from json

--- consumer.js
+++ consumer.js
@@ -5,28 +5,50 @@ const events = require('events');
 let fs = require('fs');
 
 const bucket = require('./bucket.json');
+var src_bucket = bucket.src;
+var dst_bucket = bucket.destiny;
 
 function processKafkaEntry(kafkaEntry, done) {
-	process.stdout.write(`${kafkaEntry.value}\n`);
 	let s3 = new AWS.S3();
-	// f_name = kafkaEntry.value;
-	let params = {Bucket: '42senko', Key: 'issasong.wav'};
-	let file = fs.createWriteStream('./save.wav');
+	var f_name = kafkaEntry.value.toString();
+	process.stdout.write('GETTING: ' + f_name + '\n');
+	let params = {Bucket: src_bucket, Key: f_name};
+	let file = fs.createWriteStream('./temp/' + f_name);
+
 	s3.getObject(params).createReadStream().pipe(file);
+
 	file.on('close', function () {
-		let read_file = fs.createReadStream('./save.wav');
-		s3.putObject({
-			Bucket: '42mabucket',
-			Key: 'save.wav',
-			Body: read_file
-		}, function (err) {
-			if (err) {
-				throw err;
-				console.log('error putting object\n');
-			}
-			process.stdout.write('uploaded\n');
-		});
+		var hbjs = require('handbrake-js');
+
+		process.stdout.write('FILE RECIEVED\n');
+		hbjs.spawn({ input: './temp/' + f_name, output: 'something.m4v' })
+		  .on('error', function(err){
+		    // invalid user input, no video found etc
+		  })
+		  .on('progress', function(progress){
+		    console.log(
+		      'Percent complete: %s, ETA: %s',
+		      progress.percentComplete,
+		      progress.eta
+		    );
+		  })
+		  .on('end', function() {
+			  let read_file = fs.createReadStream('something.m4v');
+			  s3.putObject({
+				  Bucket: dst_bucket,
+				  Key: 'something.m4v',
+				  Body: read_file
+			  }, function (err) {
+				  if (err) {
+					  throw err;
+					  console.log('error putting object\n');
+				  }
+				  process.stdout.write('FILE UPLOADED\n');
+			  });
+		  });
 	});
+
+
 	// test();
 	return done();
 }
@@ -41,4 +63,4 @@ let consumer = new BackbeatConsumer({
 
 consumer.on('error', err => process.stdout.write(`${err}`));
 consumer.subscribe();
-process.stdout.write(`hello\n`);
+process.stdout.write(`CONSUMER STARTED.\n`);
