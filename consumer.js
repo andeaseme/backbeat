8be1dbf47f8486f91ea17aa772c1a4705022d8fc
Wiley Debs <wdebs@student.42.us.org>
2017-08-16 21:38:49 -0700

    Add files via upload
    Fixed issue where readstream and writestream were in a race condition.

--- consumer.js
+++ consumer.js
@@ -1,57 +1,80 @@
 const config = require('./conf/Config');
 const BackbeatConsumer = require('./lib/BackbeatConsumer');
 var AWS = require('aws-sdk'),
-    fs = require('fs');
-var sox = require('sox');
+	fs = require('fs');
+//var sox = require('sox');
 
 const bucket = require('./bucket.json');
+const send = async (rs, bucket, filename) => {
+	let s3 = new AWS.S3();
+	rs.on('open', () => {
+		console.log('OPEN');
+	});
+	rs.on('end', () => {
+		console.log('END');
+	});
+	rs.on('close', () => {
+		console.log('CLOSE');
+	});
 
-var s_buck = bucket.src;
-var d_buck = bucket.destiny;
-var f_name = "issasong.mp3";
+	const response = await s3.upload({
+		Bucket: bucket,
+		Key: filename,
+		Body: rs
+	}).promise();
+	console.log('response');
+	console.log(response);
+}
 
-var options = {
-  input: f_name,
-  output: 'something.mp4',
-  preset: 'Normal',
-  rotate: 1
+
+let s_buck = bucket.src;
+let d_buck = bucket.destiny;
+let f_name = "issasong.mp3";
+
+let options = {
+	input: f_name,
+	output: 'something.mp4',
+	preset: 'Normal',
+	rotate: 1
 }
 
 function processKafkaEntry(kafkaEntry, done) {
-    process.stdout.write(`${kafkaEntry.value}\n`);
-    var s3 = new AWS.S3();
-    // f_name = kafkaEntry.value;
-    var params = {Bucket: "kafkarawaudio", Key: 'issasong.mp3'};
-    var file = require('fs').createWriteStream('./temp/save.mp3');
-    s3.getObject(params).createReadStream().pipe(file);
-    // test();
-    return done();
+	process.stdout.write(`${kafkaEntry.value}\n`);
+	let s3 = new AWS.S3();
+	// f_name = kafkaEntry.value;
+	let params = {Bucket: '42senko', Key: 'issasong.wav'};
+	let file = require('fs').createWriteStream('./save.wav');
+	let read_file = fs.createReadStream('./save.wav');
+	s3.getObject(params).createReadStream().pipe(file);
+	send(read_file, '42mabucket', 'save.wav').catch(err =>{
+		console.log(err);
+	});
+	// test();
+	return done();
 }
 
-function test()
-{
+/*function test()
+  {
   var hbjs = require('handbrake-js');
   hbjs.spawn({ input: 'issashow.avi', output: 'something.m4v' })
-    .on('error', function(err){
-      // invalid user input, no video found etc
-    })
-    .on('progress', function(progress){
-      console.log(
-        'Percent complete: %s, ETA: %s',
-        progress.percentComplete,
-        progress.eta
-      );
-    });
-}
+  .on('error', function(err){
+// invalid user input, no video found etc
+})
+.on('progress', function(progress){
+console.log(
+'Percent complete: %s, ETA: %s',
+progress.percentComplete,
+progress.eta
+);
+});
+}*/
 
-
-
-const consumer = new BackbeatConsumer({
-    zookeeper: config.zookeeper,
-    topic: 'producer-test-topic',
-    groupId: config.extensions.replication.queueProcessor.groupId,
-    concurrency: 1, // Process one entry at a time.
-    queueProcessor: processKafkaEntry,
+let consumer = new BackbeatConsumer({
+	zookeeper: config.zookeeper,
+	topic: 'producer-test-topic',
+	groupId: config.extensions.replication.queueProcessor.groupId,
+	concurrency: 1, // Process one entry at a time.
+	queueProcessor: processKafkaEntry,
 });
 
 consumer.on('error', err => process.stdout.write(`${err}`));
