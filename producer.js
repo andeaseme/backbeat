f810591f1cbbc00ba2eca92dd50636aa5aba2c38
Wiley Debs <wdebs@student.42.us.org>
2017-08-21 16:49:53 -0700

    made syntactical changes, committing before many changes

--- producer.js
+++ producer.js
@@ -1,17 +1,17 @@
 const config = require('./conf/Config');
 const BackbeatProducer = require('./lib/BackbeatProducer');
 const uuidv4 = require('uuid/v4');
-var AWS = require('aws-sdk'),
+let AWS = require('aws-sdk'),
 	fs = require('fs');
 
 const bucket = require('./bucket.json');
-var src_bucket = bucket.src;
-var f_name = process.argv[2];
+let src_bucket = bucket.src;
+let f_name = process.argv[2];
 
-var params1 = {
- Bucket: bucket.src,
- Prefix : "raw/",
- MaxKeys: 2
+let params1 = {
+	Bucket: bucket.src,
+	Prefix : "raw/",
+	MaxKeys: 2
 };
 
 function check()
@@ -24,46 +24,48 @@ function check()
 		{
 			console.log('FOUND');
 
-var raw_name = data.Contents[1].Key;
-let ready_name = 'ready/' + data.Contents[1].Key.substring(4);
- let params = {
-  Bucket: bucket.src,
-  CopySource: bucket.src + '/' + data.Contents[1].Key,
-  Key: ready_name
- };
- console.log("WILL BE DELETING OBJECT: " + raw_name);           // successful response
+			let raw_name = data.Contents[1].Key;
+			let ready_name = 'ready/' + data.Contents[1].Key.substring(4);
+			let params = {
+				Bucket: bucket.src,
+				CopySource: bucket.src + '/' + data.Contents[1].Key,
+				Key: 'ready/' + data.Contents[1].Key.substring(4)
+			};
+			console.log("WILL BE DELETING OBJECT: " + raw_name);           // successful response
 
- console.log('PARAMS:' + params.CopySource);
- s3.copyObject(params, (err, data) => {
-   if (err) console.log(err, err.stack); // an error occurred
-   else     {
-			 console.log("DELETING OBJECT: " + raw_name);           // successful response
+			console.log('PARAMS:' + params.CopySource);
+			s3.copyObject(params, (err, data) => {
+				if (err)
+					console.log(err, err.stack); // an error occurred
+				else {
+					console.log("DELETING OBJECT: " + raw_name);           // successful response
 
-			 let params = {
-	  Bucket: bucket.src ,
-	  Key: raw_name
-	 };
-	 s3.deleteObject(params, function(err, data) {
-	   if (err) console.log(err, err.stack); // an error occurred
-	   else
-		 {
-			 send(ready_name, 0);
-			 send("Done",0);
-		 }
+					let params = {
+						Bucket: bucket.src ,
+						Key: raw_name
+					};
+					s3.deleteObject(params, function(err, data) {
+						if (err)
+							console.log(err, err.stack); // an error occurred
+						else
+						{
+							send(ready_name, 0);
+							send("Done",0);
+						}
 
-
-	 });
- }
- });
+					});
+				}
+			});
 		}
-		else {
-			 send("Done",0);
+		else
+		{
+			send("Done",0);
 		}
 	});
 }
 
 if (process.argv.length == 2) {
-		setInterval(check, 1000);
+	setInterval(check, 1000);
 }
 else if (process.argv.length == 3)
 {
@@ -72,7 +74,7 @@ else if (process.argv.length == 3)
 
 function upload()
 {
-	var fileStream = fs.createReadStream(f_name);
+	let fileStream = fs.createReadStream(f_name);
 
 	fileStream.on('error', function (err) {
 		if (err) { throw err; }
@@ -80,9 +82,9 @@ function upload()
 
 	fileStream.on('open', function () {
 		f_name = f_name.substring(0, f_name.lastIndexOf('.')) + '-' +
-				 uuidv4() + f_name.substring(f_name.lastIndexOf('.'),
-						 f_name.length);
-		var s3 = new AWS.S3();
+			uuidv4() + f_name.substring(f_name.lastIndexOf('.'),
+					f_name.length);
+		let s3 = new AWS.S3();
 		s3.putObject({
 			Bucket: src_bucket,
 			Key: 'ready/' + f_name,
